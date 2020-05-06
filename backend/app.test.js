46bf6fc39298bc3a7d02f886ec11253af693772b
Josh Blumberg <jlblumberg@gmail.com>
2020-05-06 08:58:10 +0100

    Add test for post's details

--- backend/app.test.js
+++ backend/app.test.js
@@ -7,7 +7,7 @@ describe('MessageApp', () => {
 
   beforeEach(() => {
     testApp = new MessageApp;
-  })
+  });
 
   it('has messages', () => {
     expect(testApp.messages).to.be.an('array');
@@ -16,5 +16,12 @@ describe('MessageApp', () => {
   it('can make a most', () => {
     testApp.post('Hello, World')
     expect(testApp.messages.length).to.equal(1)
+  });
+
+  it('posts have content, date, and id', () => {
+    testApp.post('Hello, World')
+    expect(testApp.messages[0].content).to.equal('Hello, World');
+    expect(testApp.messages[0].date).not.to.equal(undefined);
+    expect(testApp.messages[0].id).to.equal(0);
   });
 });
