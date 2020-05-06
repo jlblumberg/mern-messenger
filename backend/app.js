33e3366956a3c6ecfd5b45b84dba50eaa93a8ed7
Josh Blumberg <jlblumberg@gmail.com>
2020-05-06 08:52:40 +0100

    Add functionality for making a new post

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,17 @@ class MessageApp {
   constructor() {
     this.messages = [];
   };
+
+  // C in CRUD
+  post(content) {
+    let item = {
+      id: this.messages.length,
+      content: content,
+      date: new Date()
+    };
+    this.messages.push(item);
+    return this.messages;
+  };
 };
 
 export default MessageApp
--- backend/app.test.js
+++ backend/app.test.js
@@ -2,8 +2,19 @@ import { expect } from 'chai';
 import MessageApp from './app.js';
 
 describe('MessageApp', () => {
-  let testApp = new MessageApp;
+
+  let testApp;
+
+  beforeEach(() => {
+    testApp = new MessageApp;
+  })
+
   it('has messages', () => {
     expect(testApp.messages).to.be.an('array');
+  });
+
+  it('can make a most', () => {
+    testApp.post('Hello, World')
+    expect(testApp.messages.length).to.equal(1)
   });
 });
