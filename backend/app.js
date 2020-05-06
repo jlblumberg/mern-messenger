7bad50e8520fb944086478e0c379ebb9925b20c1
Josh Blumberg <jlblumberg@gmail.com>
2020-05-06 09:56:30 +0100

    Refactor MessageApp to have non-zero and unique IDs

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ class MessageApp {
   // C in CRUD
   post(content) {
     let item = {
-      id: this.messages.length,
+      id: newId(this.messages),
       content: content,
       date: new Date()
     };
@@ -16,21 +16,29 @@ class MessageApp {
 
   // R
   get(id) {
-    return this.messages[id];
+    return this.messages.filter(message => message.id === id)[0];
   };
 
   // U
   update(id, updatedContent) {
-    this.messages[id].content = updatedContent;
-    return this.messages[id];
+    let index = this.messages.findIndex(message => message.id === id);
+    this.messages[index].content = updatedContent;
   };
 
   // D
   delete(id) {
-    this.messages.splice(id - 1, 1);
-    return this.messages;
+    this.messages = this.messages.filter(message => message.id != id)
+    return this.messages
   };
 
+};
+
+const newId = (array) => {
+  if (array.length > 0) {
+    return array[array.length - 1].id + 1;
+  } else {
+    return 1
+  };
 };
 
 export default MessageApp
--- backend/app.test.js
+++ backend/app.test.js
@@ -22,21 +22,46 @@ describe('MessageApp', () => {
   it('posts have content, date, and id', () => {
     expect(testApp.messages[0].content).to.equal('Hello, World');
     expect(testApp.messages[0].date).not.to.equal(undefined);
-    expect(testApp.messages[0].id).to.equal(0);
+    expect(testApp.messages[0].id).to.equal(1);
   });
 
   it('can read posts', () => {
-    expect(testApp.get(0).content).to.equal('Hello, World');
+    expect(testApp.get(1).content).to.equal('Hello, World');
   });
 
   it('can update posts', () => {
-    testApp.update(0, 'Hi, World');
+    testApp.update(1, 'Hi, World');
     expect(testApp.messages[0].content).to.equal('Hi, World');
   });
 
   it('can delete posts', () => {
-    testApp.delete(0);
+    testApp.delete(1);
     expect(testApp.messages.length).to.equal(0);
+  });
+
+  it("id's are always unique", () => {
+    testApp.post('1')
+    testApp.post('2')
+    testApp.delete(1)
+    testApp.post('3')
+    expect(testApp.messages[1].id).to.equal(3)
+  });
+
+  it("app deletes correctly", () => {
+    testApp.post('1')
+    testApp.post('2')
+    testApp.post('3')
+    testApp.delete(0)
+    testApp.delete(2)
+    expect(testApp.get(1).id).to.equal(1)
+  });
+
+  it("app updates correctly", () => {
+    testApp.post('1')
+    testApp.post('2')
+    testApp.delete(1)
+    testApp.update(2, 'update')
+    expect(testApp.get(2).content).to.equal('update')
   });
 
 });
