0d2664ffe9e69b7c9bc635c7ea5771f85584d599
Josh Blumberg <jlblumberg@gmail.com>
2020-05-18 08:55:31 +0100

    Add snapshot test for messageapp

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'React';
+import ReactDOM from 'react-dom';
+import MessageApp from './App';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { mount } from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('App', () => {
+
+  const component = mount(<MessageApp />);
+
+  it('renders correctly', () => {
+    expect(component).toMatchSnapshot();
+  });
+
+})
