dcdd9687eda32315d7c7cf2be6205d73b76fee61
Josh Blumberg <jlblumberg@gmail.com>
2020-05-19 09:11:10 +0100

    Add ErrorHandler component

---
+++ frontend/src/__tests__/errorHandler.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ErrorHandler from '../components/errorHandler';
+import errorMock from '../__mocks__/error.json'
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { mount, shallow } from 'enzyme';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Error', () => {
+  it('renders correctly', () => {
+    const component = mount(<ErrorHandler/>);
+    expect(component).toMatchSnapshot();
+  })
+})
